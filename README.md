864573aaeeb1c0fe340ffd26a2f014e63e458bd9
Suraj Gaud <75672977+suraj-xd@users.noreply.github.com>
2023-09-13 12:46:53 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# 📶 Auto-Connect-Wifi (Rungta-College, Bhilai)
+
+Automate the process of connecting to your college Wi-Fi effortlessly, without any user interaction.
+
+## Installation
+1. **Prerequisites:** Ensure you have the [TamperMonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) browser extension installed.
+
+2. **Installation:** Click the link below to install the script:
+   [![Install Script](https://img.shields.io/badge/Install-Script-blue.svg)](https://github.com/suraj-xd/auto-connect-wifi/raw/master/main.user.js)
+
+## Links
+- GitHub Repository: [suraj-xd/auto-connect-wifi](https://github.com/suraj-xd/auto-connect-wifi)
+
+---
+
+Enjoy seamless Wi-Fi connectivity with just one click. Say goodbye to manual connections! 😎
+
+Wishing you a fantastic day! 🌟
---
+++ main.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Auto Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Auto fill username and password and click login button
+// @author       You
+// @match        http://10.10.10.252:8090/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=10.252
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Function to fill the username and password fields and submit the form
+  function autoLogin() {
+      const username = "Rungtar1"; // Replace with your username
+      const password = "Tech9900"; // Replace with your password
+
+      const usernameInput = document.querySelector('input[name="username"]');
+      const passwordInput = document.querySelector('input[name="password"]');
+      const loginButton = document.querySelector('input[name="btnSubmit"]');
+
+      if (usernameInput && passwordInput && loginButton) {
+          // Fill in the username and password fields
+          usernameInput.value = username;
+          passwordInput.value = password;
+
+          // Click the login button to submit the form
+          loginButton.click();
+      }
+  }
+
+  // Run the autoLogin function when the page is loaded
+  window.addEventListener('load', autoLogin);
+})();
